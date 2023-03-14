058f1480c5161b4e1d16ae21666c8948e4538ab6
Praveen Kumar Verma <kr.praveen.infinity@gmail.com>
2023-03-14 08:26:30 +0530

    created new components - initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
+import { OrderComponent } from './core/order/order.component';
+import { MenuComponent } from './core/order/menu/menu.component';
+import { CustomerDetailsComponent } from './core/order/customer-details/customer-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent,
+    OrderComponent,
+    MenuComponent,
+    CustomerDetailsComponent
   ],
   imports: [
     BrowserModule,
