f326be0842b72af98d1ff8480a7266de1c041c07
Praveen Kumar Verma <kr.praveen.infinity@gmail.com>
2023-03-14 12:06:05 +0530

    added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoreComponent } from './core/core.component';
+import { CustomerDetailsComponent } from './core/order/customer-details/customer-details.component';
+import { OrderComponent } from './core/order/order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CoreComponent},
+  {path: 'order', component: OrderComponent},
+  {path: 'customer-details', component: CustomerDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialCustomModule } from '../material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +12,7 @@ import { CoreComponent } from './core/core.component';
 import { OrderComponent } from './core/order/order.component';
 import { MenuComponent } from './core/order/menu/menu.component';
 import { CustomerDetailsComponent } from './core/order/customer-details/customer-details.component';
+import { MenuItemComponent } from './core/order/menu/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +20,18 @@ import { CustomerDetailsComponent } from './core/order/customer-details/customer
     CoreComponent,
     OrderComponent,
     MenuComponent,
-    CustomerDetailsComponent
+    CustomerDetailsComponent,
+    MenuItemComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule, 
+    MaterialCustomModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
