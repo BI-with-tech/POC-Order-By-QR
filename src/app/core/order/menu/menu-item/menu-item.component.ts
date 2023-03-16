c962aa5c25c7840518fb1a7f42217e2969d81811
Praveen Kumar Verma <kr.praveen.infinity@gmail.com>
2023-03-16 20:07:57 +0530

    add-subtract buttons added

--- src/app/core/order/menu/menu-item/menu-item.component.ts
+++ src/app/core/order/menu/menu-item/menu-item.component.ts
@@ -9,4 +9,33 @@ export class MenuItemComponent {
   @Input() menuItem: any;
 
   panelOpenState = false;
+  orderArray = new Map();
+
+  addItem(itemCode: string){
+    if(this.orderArray.has(itemCode)){
+      this.orderArray.set(itemCode, this.orderArray.get(itemCode)+1);
+    }
+    else{
+      this.orderArray.set(itemCode, 1);
+    }
+    console.log(this.orderArray);
+  }
+
+  subtractItem(itemCode: string){
+    if(this.orderArray.has(itemCode)){
+      this.orderArray.set(itemCode, this.orderArray.get(itemCode)-1);
+      if(this.orderArray.get(itemCode) == 0){
+        this.orderArray.delete(itemCode);
+      }
+    }
+    console.log(this.orderArray);
+  }
+
+  getItemCount(itemCode: string){
+    if(this.orderArray.has(itemCode)){
+      return this.orderArray.get(itemCode);
+    }
+    else
+      return 0;
+  }
 }
